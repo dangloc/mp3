f07c817fb2d8530281cba105bb8ad2fe3bd1690f
DangBaLoc <dangloc522@gmail.com>
2023-04-28 22:14:44 +0700

    setup API fetch API vs AXIOS

--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Header } from '../../components'
+import * as apis from '../../apis'
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const response  = await apis.getHome()
+      console.log(response)
+    }
+    fetchData()
+  }, [])
   return (
     <div className='overflow-y-auto'>
       <dir className="h-[70px] px-[59px] flex items-center">
